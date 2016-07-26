e49bceb8d7e2e453ffc6cbd598735b8da6a5d96c
rafael <rafael@engsolutions.com.br>
2016-07-26 10:31:17 -0300

    using let instead var and change some loopings

--- devBox.Linq.js
+++ devBox.Linq.js
@@ -1,7 +1,7 @@
 module.exports = lisqJs(); 
 
 function lisqJs() {
-    var Array = global.Array;
+    let Array = global.Array;
 
     function ToList(lista) {
         this.Array = lista;
@@ -16,7 +16,7 @@ function lisqJs() {
         this.Condition = [];
 
         if (conditon !== undefined) {
-            for (var i = 0; i < conditon.length; i++) {
+            for (let i = 0; i < conditon.length; i++) {
                 this.Condition.push(conditon[i]);
             }
         }
@@ -30,8 +30,8 @@ function lisqJs() {
             this.Groups.push(group);
         }
         function getKeys(group) {
-            var list = [];
-            for (var i = 0; i < this.Groups.length; i++)
+            let list = [];
+            for (let i = 0; i < this.Groups.length; i++)
                 list.push(this.Groups[i].Key);
 
             return list;
@@ -42,8 +42,8 @@ function lisqJs() {
         this.Key = keyValue;
     }
     function getProperties(obj) {
-        var properties = [];
-        for (var prop in obj) {
+        let properties = [];
+        for (let prop in obj) {
             if (typeof obj[prop] != 'function') {
                 properties.push(prop);
             }
@@ -51,11 +51,11 @@ function lisqJs() {
         return properties;
     }
     EnumOrderBy.prototype.ThenBy = function(conditon) {
-        var a = this.Condition;
+        let a = this.Condition;
 
-        var list = this.Array.OrderBy(conditon).ToList();
+        let list = this.Array.OrderBy(conditon).ToList();
 
-        for (var i = a.length - 1; i >= 0; i--) {
+        for (let i = a.length - 1; i >= 0; i--) {
             list = list.OrderBy(a[i]).ToList();
         }
         this.Condition.push(conditon);
@@ -63,11 +63,11 @@ function lisqJs() {
         return new EnumOrderBy(list, this.Condition);
     };
     EnumOrderBy.prototype.ThenByDesc = function(conditon) {
-        var a = this.Condition;
+        let a = this.Condition;
 
-        var list = this.Array.OrderByDesc(conditon).ToList();
+        let list = this.Array.OrderByDesc(conditon).ToList();
 
-        for (var i = a.length - 1; i >= 0; i--) {
+        for (let i = a.length - 1; i >= 0; i--) {
             list = list.OrderByDesc(a[i]).ToList();
         }
         this.Condition.push(conditon)
@@ -80,10 +80,10 @@ function lisqJs() {
         });
     };
     Array.prototype.OrderBy = function(condition) {
-        for (var i = 0; i < this.length; i++) {
-            for (var j = i + 1; j < this.length; j++) {
+        for (let i = 0; i < this.length; i++) {
+            for (let j = i + 1; j < this.length; j++) {
                 if (condition(this[i]) > condition(this[j])) {
-                    var a = this[j];
+                    let a = this[j];
                     this[j] = this[i];
                     this[i] = a;
                 }
@@ -92,10 +92,10 @@ function lisqJs() {
         return new EnumOrderBy(this, [condition]);
     };
     Array.prototype.OrderByDesc = function(condition) {
-        for (var i = 0; i < this.length; i++) {
-            for (var j = i + 1; j < this.length; j++) {
+        for (let i = 0; i < this.length; i++) {
+            for (let j = i + 1; j < this.length; j++) {
                 if (condition(this[i]) < condition(this[j])) {
-                    var a = this[j];
+                    let a = this[j];
                     this[j] = this[i];
                     this[i] = a;
                 }
@@ -109,25 +109,19 @@ function lisqJs() {
             return;
         }
 
-        var list = [];
-        for (var i = 0; i < this.length; i++) {
-            if (condition(this[i])) {
-                list.push(this[i]);
-            }
-        }
-        return list;
+        return list.filter(condition);
     };
     Array.prototype.Any = function(condition) {
         if (condition === undefined) {
             return this.length > 0;
         }
-        for (var i = 0; i < this.length; i++) {
+        for (let i = 0; i < this.length; i++) {
             if (condition(this[i]))
                 return true;
         }
         return false;
 
-        //var cond = false;
+        //let cond = false;
         //this.For(function (i , obj) {
         //    if (condition(obj))
         //        cond = true;
@@ -139,7 +133,7 @@ function lisqJs() {
             return;
         }
 
-        for (var i = 0; i < this.length; i++) {
+        for (let i = 0; i < this.length; i++) {
             if (!condition(this[i]))
                 return false;
         }
@@ -151,7 +145,7 @@ function lisqJs() {
             return;
         }
 
-        for (var i = 0; i < this.length; i++) {
+        for (let i = 0; i < this.length; i++) {
             if (condition(this[i]))
                 return this[i];
         }
@@ -165,12 +159,8 @@ function lisqJs() {
             console.error('Informe a expressão para executar a tarefa.');
             return;
         }
-
-        var list = [];
-        for (var i = 0; i < this.length; i++) {
-            list.push(condition(this[i]));
-        }
-        return list;
+	
+	return this.map(condition)
     };
     Array.prototype.Remove = function(condition) {
         if (condition == undefined) {
@@ -178,7 +168,7 @@ function lisqJs() {
             return;
         }
 
-        for (var i = 0; i < this.length; i++) {
+        for (let i = 0; i < this.length; i++) {
             if (condition(this[i]))
                 this.splice(i, 1);
         }
@@ -189,7 +179,7 @@ function lisqJs() {
                 ? null
                 : attr(this[0])
 
-        for (var i = 0; i < this.length; i++) {
+        for (let i = 0; i < this.length; i++) {
             if (condition(this[i]))
                 return attr(this[i]);
         }
@@ -208,28 +198,15 @@ function lisqJs() {
         if (skip > this.length)
             skip = this.length;
 
-        var list = [];
-
-        for (var i = skip; i < this.length; i++) {
-            list.push(this[i]);
-        }
-        return list;
+	return this.slice(skip);
     };
     Array.prototype.Take = function(take) {
         if (take == undefined || isNaN(take)) {
             console.error('Informe um valor válido.');
             return;
         }
-        if (take > this.length) {
-            take = this.length;
-        }
-
-        var list = [];
-
-        for (var i = 0; i < take; i++) {
-            list.push(this[i]);
-        }
-        return list;
+	
+        return this.slice(0, take);
     };
     Array.prototype.For = function(callback, indice) {
         if (callback == undefined) {
@@ -241,16 +218,16 @@ function lisqJs() {
         if (indice > this.length)
             indice = this.length;
 
-        var list = [];
+        let list = [];
 
-        for (var i = indice; i < this.length; i++) {
+        for (let i = indice; i < this.length; i++) {
             callback(i, this[i]);
         }
     };
     Array.prototype.Sum = function(conditon) {
-        var list = this.Select(conditon);
+        let list = this.Select(conditon);
 
-        var sum = 0;
+        let sum = 0;
 
         list.For(function(i, obj) {
             sum += obj;
@@ -259,7 +236,7 @@ function lisqJs() {
         return sum;
     };
     Array.prototype.Max = function(conditon) {
-        var list = this.Select(conditon).Order();
+        let list = this.Select(conditon).Order();
 
         return list[list.length - 1];
     };
@@ -267,16 +244,16 @@ function lisqJs() {
         if (this.length <= 0)
             return [];
 
-        var list = new EnumGroupBy();
+        let list = new EnumGroupBy();
 
         while (this.length > 0) {
-            var a = conditon(this[0])
-            var c = [];
-            for (var i = 0; i < this.length; i++) {
+            let a = conditon(this[0])
+            let c = [];
+            for (let i = 0; i < this.length; i++) {
                 if (conditon(this[i]) == a)
                     c.push(this[i]);
             }
-            for (var j = 0; j < this.length; j++) {
+            for (let j = 0; j < this.length; j++) {
                 if (conditon(this[j]) == a)
                     this.splice(j, 1);
             }
@@ -285,15 +262,15 @@ function lisqJs() {
         return list;
     };
     Array.prototype.Distinct = function() {
-        var l = [];
-        for (var i = 0; i < this.length; i++) {
-            for (var k = i + 1; k < this.length; k++) {
+        let l = [];
+        for (let i = 0; i < this.length; i++) {
+            for (let k = i + 1; k < this.length; k++) {
                 if (dist(this[i], this[k]))
                     l.push({ i1: i, i2: k });
             }
         }
-        var la = [];
-        for (var g = 0; g < l.length; g++) {
+        let la = [];
+        for (let g = 0; g < l.length; g++) {
             if (la.indexOf(l[g].i2) == -1) {
                 la.push(l[g].i2);
                 this.splice(l[g].i2 - i, 1);
@@ -305,9 +282,9 @@ function lisqJs() {
             if (obj1.length > 0) {
                 return distList(obj1, obj2);
             }
-            var a = getProperties(obj1);
-            var bl = false;
-            for (var j = 0; j < a.length; j++) {
+            let a = getProperties(obj1);
+            let bl = false;
+            for (let j = 0; j < a.length; j++) {
                 if (typeof obj1[a[j]] == 'object') {
                     bl = dist(obj1[a[j]], obj2[a[j]]);
                     if (!bl) {
@@ -328,7 +305,7 @@ function lisqJs() {
             if (obj1.length != obj2.length)
                 return false;
 
-            for (var i = 0; i < obj1.length; i++) {
+            for (let i = 0; i < obj1.length; i++) {
                 if (!dist(obj1[i], obj2[i]))
                     return false;
             }
@@ -342,7 +319,7 @@ function lisqJs() {
         return this.Where(conditon).length;
     };
     Array.prototype.LastOrDefault = function(conditon) {
-        for (var i = this.length - 1; i > 0; i--) {
+        for (let i = this.length - 1; i > 0; i--) {
             if (condition(this[i]))
                 return this[i];
         }
@@ -353,12 +330,10 @@ function lisqJs() {
         return this[this.length - 1];
     };
     Array.prototype.AddRange = function(array) {
-        for (var i = 0; i < array.length; i++) {
-            this.push(array[i]);
-        }
+        return x.concat(array);
     };
     Array.prototype.Exist = function(conditon) {
-        for (var i = 0; i < this.length; i++) {
+        for (let i = 0; i < this.length; i++) {
             if (conditon(this[i]))
                 return true;
         }
